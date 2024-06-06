13313973e20656c849446b28237eb2517fe29068
Dragos Tanta <dragos.tanta@gmail.com>
2024-06-06 10:08:40 +0300

    DummyCommit

---
+++ src/main/java/tests/GitResetCommands.java
@@ -0,0 +1,21 @@
+package tests;
+
+public class GitResetCommands {
+	
+	
+	
+	/*
+	 *  1. Reset all files from staging area
+	 *  git reset  (muta toate fisierele din staging inapoi in working dir)
+	 * 
+	 *  2. Reset single file from stanging area
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
