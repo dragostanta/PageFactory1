34fc9f4bc579b9ab948045e6af88e75680785151
Dragos Tanta <dragos.tanta@gmail.com>
2024-06-10 10:47:16 +0300

    curs27 log4J

--- src/main/java/selenium/utils/SeleniumWrappers.java
+++ src/main/java/selenium/utils/SeleniumWrappers.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.TestException;
 
+import framework.utils.Log;
+
+
 public class SeleniumWrappers extends BaseTest{
 
 	
@@ -19,31 +22,46 @@ public class SeleniumWrappers extends BaseTest{
 	
 	
 	public void click(WebElement element) {
-		
+		Log.info("called <click()> on element: " + element);
 		try {
 			WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+			Log.info("called <waitForElementToBeClickable> on element: " + element);
 			wait.until(ExpectedConditions.elementToBeClickable(element));
 			element.click();
 			
 		}catch(StaleElementReferenceException e) {
+			Log.error("StaleElementException on element: " + element);
 			WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
-			wait.until(ExpectedConditions.elementToBeClickable(element));
+			Log.error("called <waitForStalenessOf> element: " + element);
+			wait.until(ExpectedConditions.stalenessOf(element));
+			Log.info("called <click()> on element: " + element);
 			element.click();
-		}catch (NoSuchElementException e) {
-			System.out.println("Element not found " + element.getAttribute("outerHTML"));
-			throw new TestException(e.getMessage());
+		}catch (Exception e) {
+			Log.error("Exception occured on " + element.getAttribute("outerHTML"));
+			Log.error("Exception occured on " + element);
+			Log.error(e.getMessage());
+			throw new TestException("unable to <click()> on element: " +element);
 			
-		}	
+		}
 	}
 	
 	
 	public void sendKeys(WebElement element, String textToWrite) {
-		
-		System.out.println("Called method <sendKeys> on element " + element);
-		WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
-		wait.until(ExpectedConditions.visibilityOf(element));
-		element.clear();
-		element.sendKeys(textToWrite);
+		Log.info("Called method <sendKeys> on element " + element);
+		try {
+			WebDriverWait wait =  new WebDriverWait(driver, Duration.ofSeconds(10));
+			Log.info("called <waitForVisibilityOfElement()> on element: " + element);
+			wait.until(ExpectedConditions.visibilityOf(element));
+			Log.info("called <clear()> on element: " + element);
+			element.clear();
+			element.sendKeys(textToWrite);
+			
+		}catch(Exception e) {
+			Log.error("Exception occured on <sendKeys()> on element: " + element);
+			Log.error(e.getMessage());
+			throw new TestException("unable to <sendKeys()> on element: " +element);
+		}
+
 		
 	}
 	
